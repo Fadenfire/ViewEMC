bc4158678db58ae2f771c233a5511f5f26f40fb3
Silly511 <remy555@icloud.com>
2016-03-14 12:16:09 -0500

    Stuff

--- src/main/java/net/sparklepopprograms/viewemc/ViewEMCMod.java
+++ src/main/java/net/sparklepopprograms/viewemc/ViewEMCMod.java
@@ -16,16 +16,21 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.sparklepopprograms.core.DimensionalCore;
+import net.sparklepopprograms.core.updatechecker.UpdateManager;
 import cpw.mods.fml.common.Mod.EventHandler;
 
-@Mod(modid = "ViewEMC", name = "View EMC", version = "v1", dependencies = ViewEMCMod.dependencies)
+@Mod(modid = "ViewEMC", name = "View EMC", version = ViewEMCMod.version, dependencies = ViewEMCMod.dependencies)
 public class ViewEMCMod {
 	
 	public static final String dependencies = "required-after:DimensionalCore@[1.0.7,);required-after:Waila@[1.5.10,);required-after:ProjectE@[PE1.9.5,)";
+	public static final String version = "v2";
 
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
 		FMLInterModComms.sendMessage("Waila", "register", "net.sparklepopprograms.viewemc.WailaHandler.callbackRegister");
+		
+		UpdateManager.register("View EMC", DimensionalCore.updateURL + "view-emc/files", 4, this.version);
 		
 	}
 }
