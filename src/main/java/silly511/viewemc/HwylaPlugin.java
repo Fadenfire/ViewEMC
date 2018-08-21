0fbc025ad20378624575a09ec805f926ea0691d4
Silly511 <13701628+Silly511@users.noreply.github.com>
2018-08-21 11:16:29 -0500

    Update to v7

--- src/main/java/silly511/viewemc/HwylaPlugin.java
+++ src/main/java/silly511/viewemc/HwylaPlugin.java
@@ -1,5 +1,6 @@
 package silly511.viewemc;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import mcp.mobius.waila.api.IWailaConfigHandler;
@@ -18,20 +19,37 @@ import net.minecraft.util.text.TextFormatting;
 @WailaPlugin
 public class HwylaPlugin implements IWailaPlugin, IWailaDataProvider {
 	
+	public static Method getEMCValue;
+	
 	@Override
 	public void register(IWailaRegistrar registrar) {
-		registrar.addConfig("EMC", "emc.shiftToggleEMC", false);
+		registrar.addConfig("EMC", "emc.shiftToggle", false);
 		
 		registrar.registerBodyProvider(this, Block.class);
+		
+		try {
+			getEMCValue = EMCHelper.class.getDeclaredMethod("getEmcValue", ItemStack.class);
+		} catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
 	}
 	
 	@Override
 	public List<String> getWailaBody(ItemStack item, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-		if ((!config.getConfig("emc.shiftToggleEMC") || accessor.getPlayer().isSneaking()) && EMCHelper.doesItemHaveEmc(item))
+		if ((!config.getConfig("emc.shiftToggleEMC") || accessor.getPlayer().isSneaking()) && EMCHelper.doesItemHaveEmc(item)) {
+			long emc;
+			
+			try { //I have to do this because ProjectE changed EMC from int to long and I want to support both versions.
+				emc = ((Number) getEMCValue.invoke(null, item)).longValue();
+			} catch (Exception ex) {
+				throw new IllegalStateException(ex);
+			}
+			
 			tooltip.add(TextFormatting.YELLOW + I18n.format("pe.emc.emc_tooltip_prefix") + " " +
-					TextFormatting.WHITE + Constants.EMC_FORMATTER.format(EMCHelper.getEmcValue(item)) +
+					TextFormatting.WHITE + Constants.EMC_FORMATTER.format(emc) +
 					TextFormatting.BLUE + EMCHelper.getEmcSellString(item, 1));
-
+		}
+		
 		return tooltip;
 	}
 
--- src/main/java/silly511/viewemc/ViewEMCMod.java
+++ src/main/java/silly511/viewemc/ViewEMCMod.java
@@ -2,5 +2,5 @@ package silly511.viewemc;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "viewemc", name = "View EMC", version = "v6", dependencies = "required-after:waila;required-after:projecte")
+@Mod(modid = "viewemc", name = "View EMC", version = "v7", dependencies = "required-after:waila;required-after:projecte")
 public class ViewEMCMod {}
